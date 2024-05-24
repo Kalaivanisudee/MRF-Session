dff5e60f60210bda52d634c9cc79ef9167a473ec
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-24 15:16:08 +0530

    reduce method added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://cors-anywhere.herokuapp.com/gitlab.com/gvanderput/gerard-movie-filtering/-/raw/master/data/movies.json"
+const apiUrl = "./data.json"
 
 async function getData(api,callbackdata){
     const res=await fetch(api);
@@ -8,18 +8,45 @@ async function getData(api,callbackdata){
 function displayData(data){
     console.log(data)
     const withoutDescription=data.map((value,index,array)=>{
-        console.log("My value:", value);
+        // console.log("My value:", value);
         let obj={...value}
-        console.log("My Cloned Object",obj);
+        // console.log("My Cloned Object",obj);
         delete obj["description"];
         return obj;
     })
     console.log(withoutDescription);
     let released2020 =data.filter((value,index,array)=>value.year==2020);
     console.log("Released Movie List",released2020);
-    //Filter Method using index 
+    //  Filter Method using index 
     const releasedMovie2020=withoutDescription.filter((value,index,array)=> index %2 ==0)
     console.log("Filter method using index",releasedMovie2020);
+    
+//    Reduce method-1
+   data.reduce((accum,value,index)=>{
+    console.log("my accumalor",accum);
+    console.log("my value", value);
+    return index;
+   },0) 
+// Reduce method 2
+let released2020Votes = released2020.reduce((accum,value,index)=>{
+    return accum+value.votes;
+},0)
+console.log("sum of Votes", released2020Votes);
+// Reduce Method - 3
+const genreObj =released2020.reduce((accum,value)=>{
+   value.genreIds.forEach(element=>{
+    if(accum[element]){
+        accum[element].push(value)
+    }else{
+        accum[element] = [value];
+    }
+    
+   })
+   return accum;
+},{})
+
+console.log("GenereID listed",genreObj);
+   
 }
 const data = getData(apiUrl, displayData);
 console.log(data);
